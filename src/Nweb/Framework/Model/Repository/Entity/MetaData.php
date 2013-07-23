03611591b415685554a376728bc032667906d207
Krzysztof Kardasz <krzysztof@kardasz.eu>
2013-07-23 08:03:16 +0200

    repository

---
+++ src/Nweb/Framework/Model/Repository/Entity/MetaData.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Nweb Framework
+ *
+ * @category    NwebFramework
+ * @package     View
+ * @author      Krzysztof Kardasz <krzysztof@kardasz.eu>
+ * @copyright   Copyright (c) 2013 Krzysztof Kardasz
+ * @license     http://www.gnu.org/licenses/lgpl-3.0.txt  GNU Lesser General Public
+ * @version     0.1-dev
+ * @link        https://github.com/nwebcode/framework
+ * @link        http://framework.nweb.pl
+ */
+
+namespace Nweb\Framework\Model\Repository\Entity;
+
+/**
+ * Application
+ *
+ * @category    NwebFramework
+ * @package     View
+ * @author      Krzysztof Kardasz <krzysztof@kardasz.eu>
+ * @copyright   Copyright (c) 2013 Krzysztof Kardasz
+ * @version     0.1-dev
+ */
+class MetaData
+{
+    /**
+     * @return mixed
+     */
+    public function columnMappings ()
+    {}
+}
